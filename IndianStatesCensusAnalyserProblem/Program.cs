0a9f3f2d5610429a2999c340e169d7bd66db592d
“AyushiSri17” <srivastavaayushi7110@gmail.com>
2023-03-11 21:48:07 +0530

    [Ayushi] Welcoming you

---
+++ IndianStatesCensusAnalyserProblem/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStatesCensusAnalyserProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Indian States Census Analyser Problem");
+            Console.ReadLine();
+        }
+    }
+}
